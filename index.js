c87d2d2ace9862344de2a90ff7acfb931618e366
Takuro Sakai <sakaccho777@gmail.com>
2018-04-08 17:37:44 +0900

    エアコンをONにする処理追加

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const lirc_node = require('lirc_node');
+
 const app = express();
- 
+lirc_node.init();
+
+
 app.get('/aircon/on', function (req, res) {
-  res.send('エアコンON');
+  lirc_node.irsend.send_once("AirCon", "Power", ()=> {
+    res.send('エアコンON');
+  });
 })
 
 app.get('/aircon/off', function (req, res) {
